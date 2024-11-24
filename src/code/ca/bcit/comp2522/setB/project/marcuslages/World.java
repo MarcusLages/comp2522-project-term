d382a8266d5e04b5e954d2f3c2a0f09ca7fea36e
MarcusLages <marcusvlages@gmail.com>
2024-11-24 00:54:15 -0800

    Read all the lines and parsed them into hashmap in World.java

--- src/code/ca/bcit/comp2522/setB/project/marcuslages/World.java
+++ src/code/ca/bcit/comp2522/setB/project/marcuslages/World.java
@@ -1,13 +1,12 @@
 package ca.bcit.comp2522.setB.project.marcuslages;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Scanner;
 
 /**
  * Class that represents a World full of countries and their
@@ -18,13 +17,11 @@ import java.util.Scanner;
  */
 public class World {
 
-    /* Data lines per country data in the txt file (counting the '\n'). */
-    private static final int DATA_LINES_PER_COUNTRY = 4;
-
-    /* Data lines per country data in the txt file (counting the '\n'). */
-    private static final int LINES_PER_COUNTRY = 5;
-    private static final int INITIAL_LINE_COUNTER = 0;
-    private static final int DIVISIBLE = 0;
+    private static final int FIRST_LINE = 0;
+    private static final int COUNTRY_NAME_IDX = 0;
+    private static final int COUNTRY_CAPITAL_IDX = 1;
+    private static final int COUNTRY_FACTS = 3;
+    private static final int FIRST_FACT_IDX = 0;
 
     /*
     * Hashmap containing all the country's objects.
@@ -44,79 +41,95 @@ public class World {
     // Method to populate the countryMap hashmap with all the countries.
     private void populateCountryMap() {
 
+        final Path folderPath;
+        folderPath = Paths.get("src","resources");
+
         for(char fileName = 'a'; fileName <= 'z'; fileName++) {
 
-            final Path currentFilepath;
             final String filenameTxtExtension;
+            final Path currentFilepath;
 
             // Resolving the filepath with the filename and its txt extension into one Path
             filenameTxtExtension = fileName + ".txt";
-            currentFilepath = Paths.get("src","resources");
-            currentFilepath.resolve(filenameTxtExtension);
+            currentFilepath = folderPath.resolve(filenameTxtExtension);
 
             if(Files.exists(currentFilepath)) {
-                // TODO: list of lines
-
+                parseCountriesToHashmap(countryMap, currentFilepath);
+                countryMap.toString();
             } else {
 
                 final String errorMsg;
-                errorMsg = filenameTxtExtension + " does not exist." + System.lineSeparator()
+                errorMsg = filenameTxtExtension + " does not exist." + System.lineSeparator();
 
                 System.out.println(errorMsg);
                 throw new RuntimeException(errorMsg);
             }
 
-            try {
-                final Scanner fileScanner;
-                fileScanner = new Scanner(countryFactsFile);
-                int lineCounter;
-                lineCounter = INITIAL_LINE_COUNTER;
-
-                while(fileScanner.hasNextLine()) {
-
-                    final Scanner lineScanner;
-                    final String currLine;
-
-                    final String countryName;
-                    final String countryCapital;
-                    final String[] countryFacts;
-
-                    countryName = null;
-                    countryCapital = null;
-                    countryFacts = new String[DATA_LINES_PER_COUNTRY];
-
-                    currLine = fileScanner.nextLine();
-
-                    if(lineCounter % LINES_PER_COUNTRY == DIVISIBLE) {
-                        // Checking first line
-                        lineScanner = new Scanner(currLine);
-                        lineScanner.useDelimiter(":");
-
-                        while(lineScanner.hasNext()) {
-                            // TODO: Be able to get data from each part of the first line
-                            final String word;
-                            word = lineScanner.next();
-
-                        }
-
-                    } if(lineCounter % LINES_PER_COUNTRY == DATA_LINES_PER_COUNTRY) {
-                        // Checks if we are the last line and adds country to HashMap.
-                        final Country currCountry;
-                        currCountry = new Country(countryName, countryCapital, countryFacts);
-                        addCountry(currCountry);
-                    } else {
-                        // Checking facts line
-                        int factIdx;
-
-                        // TODO: Check at which position we are at facts array and append to it
-                    }
-
-                    lineCounter++;
-                }
-            } catch (final Exception e) {
-                e.printStackTrace();
-            }
         }
+    }
+
+    private static void parseCountriesToHashmap(final Map<String, Country> countryMap,
+                                                final Path currentFilepath) {
+        final List<String> lines;
+
+        try {
+            lines = Files.readAllLines(currentFilepath);
+
+            int i = FIRST_LINE;
+            while (i < lines.size()) {
+
+                // Skip empty lines between entries
+                if (lines.get(i).trim().isEmpty()) {
+                    i++;
+                    continue;
+                }
+
+                // Extract country name and capital
+                final Country country;
+                final String[] nameCapital;
+                final String countryName;
+                final String capitalCityName;
+                final String[] countryFacts;
+
+                nameCapital = lines.get(i).split(":");
+                countryName = nameCapital[COUNTRY_NAME_IDX].trim();
+                capitalCityName = nameCapital[COUNTRY_CAPITAL_IDX].trim();
+
+                // Jumps to the next line, which are the country facts lines.
+                i++;
+
+                // Extract the next COUNTRY_FACTS lines as facts and jump skip those COUNTRY_FACTS lines
+                countryFacts = parseCountryFacts(lines, i);
+                i += COUNTRY_FACTS;
+
+                country = new Country(countryName, capitalCityName, countryFacts);
+
+                countryMap.put(countryName, country);
+            }
+
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String[] parseCountryFacts(final List<String> lines,
+                                              final int startLine) {
+
+        int currentLine;
+        final String[] countryFacts;
+
+        currentLine = startLine;
+        countryFacts = new String[COUNTRY_FACTS];
+
+        for (int j = FIRST_FACT_IDX; j < COUNTRY_FACTS && currentLine < lines.size(); j++) {
+
+            countryFacts[j] = lines.get(currentLine).trim();
+
+            // Next country facts line
+            currentLine++;
+        }
+
+        return countryFacts;
     }
 
     private void addCountry(final Country country) {
