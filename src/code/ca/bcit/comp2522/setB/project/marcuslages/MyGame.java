2e6e4c2ee6e77ad83fb34fd9c771f9db1968a287
MarcusLages <marcusvlages@gmail.com>
2024-11-30 14:34:32 -0800

    Implemented resettable and MyGame.java constructor

--- src/code/ca/bcit/comp2522/setB/project/marcuslages/MyGame.java
+++ src/code/ca/bcit/comp2522/setB/project/marcuslages/MyGame.java
@@ -1,17 +1,35 @@
 package ca.bcit.comp2522.setB.project.marcuslages;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyGame
-        implements Game {
+        implements Game, Resettable {
 
     private final WordDeck deck;
+    private final List<Word> userHand;
+    private final List<Word> botHand;
 
     public MyGame() {
 
         deck = new WordDeck();
+        userHand = new ArrayList<>();
+        botHand = new ArrayList<>();
+
+
     }
 
     @Override
     public void startGame() {
 
+    }
+
+    @Override
+    public void reset() {
+
+        userHand.clear();
+        botHand.clear();
+        deck.refillDeck();
+        startGame();
     }
 }
