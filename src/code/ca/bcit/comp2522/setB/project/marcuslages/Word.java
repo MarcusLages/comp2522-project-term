eab7fb30e7cff7f835b4328864b2b8da59885605
MarcusLages <marcusvlages@gmail.com>
2024-11-30 14:34:05 -0800

    Implemented Word.java

--- src/code/ca/bcit/comp2522/setB/project/marcuslages/Word.java
+++ src/code/ca/bcit/comp2522/setB/project/marcuslages/Word.java
@@ -1,4 +1,21 @@
 package ca.bcit.comp2522.setB.project.marcuslages;
 
 public class Word {
+
+    // Constant to access the rear index after
+    // getting the length of the String
+    public static final int REAR_OFFSET = 1;
+    private final static int HEAD_INDEX = 0;
+
+    private final String word;
+    private final char head;
+    private final char rear;
+
+    public Word(final String word) {
+
+        this.word = word;
+        head = word.charAt(HEAD_INDEX);
+        rear = word.charAt(word.length() - REAR_OFFSET);
+    }
+
 }
