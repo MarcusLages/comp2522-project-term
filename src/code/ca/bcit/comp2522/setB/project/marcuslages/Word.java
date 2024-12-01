afaaaaf7bb07be57039d6ed57df14bce1be235e7
MarcusLages <marcusvlages@gmail.com>
2024-12-01 10:08:15 -0800

    Implemented Word.java for representing WordCards

--- src/code/ca/bcit/comp2522/setB/project/marcuslages/Word.java
+++ src/code/ca/bcit/comp2522/setB/project/marcuslages/Word.java
@@ -4,18 +4,41 @@ public class Word {
 
     // Constant to access the rear index after
     // getting the length of the String
-    public static final int REAR_OFFSET = 1;
+    private final static int REAR_OFFSET = 1;
+    private final static int SECOND_REAR_OFFSET = 2;
     private final static int HEAD_INDEX = 0;
+    private final static int SECOND_HEAD_INDEX = 1;
 
     private final String word;
     private final char head;
     private final char rear;
 
     public Word(final String word) {
+        validateWord(word);
 
         this.word = word;
-        head = word.charAt(HEAD_INDEX);
-        rear = word.charAt(word.length() - REAR_OFFSET);
+        head = Character.toLowerCase(word.charAt(HEAD_INDEX));
+        rear = Character.toLowerCase(word.charAt(word.length() - REAR_OFFSET));
+    }
+
+    @Override
+    public String toString() {
+
+        final StringBuilder sb;
+        sb = new StringBuilder();
+
+        sb.append(Character.toUpperCase(head))
+                .append(word, SECOND_HEAD_INDEX, word.length() - SECOND_REAR_OFFSET)
+                .append(Character.toUpperCase(rear));
+
+        return sb.toString();
+    }
+
+    private static void validateWord(final String word) {
+
+        if(word == null || word.isBlank()) {
+            throw new IllegalArgumentException("Invalid word.");
+        }
     }
 
 }
